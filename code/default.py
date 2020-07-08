d28ffa43a5e01a07fe640ad66deeffa5fca947d1
Ahsen <ahsen.khaliq@gmail.com>
2020-07-08 12:31:44 -0400

    return image

--- code/default.py
+++ code/default.py
@@ -21,7 +21,9 @@ def defaultrun(images):
     from code.ProjectPaintingLight import run
 
 
-    run(image, mask, ambient_intensity, light_intensity, light_source_height,
+    imageout = run(image, mask, ambient_intensity, light_intensity, light_source_height,
         gamma_correction, stroke_density_clipping, light_color_red, light_color_green,
         light_color_blue, enabling_multiple_channel_effects)
+
+    return imageout
 
