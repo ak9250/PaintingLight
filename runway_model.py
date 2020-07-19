f95fdff79a52710d815cb71cfd9256051bc08240
Ahsen <ahsen.khaliq@gmail.com>
2020-07-18 23:14:51 -0400

    recommended defaults ;

--- runway_model.py
+++ runway_model.py
@@ -9,13 +9,13 @@ from code import default as dt
 manipulate_command_inputs = {
     'image': runway.image(description='The input image containing a face to transform.'),
     'ambient_intensity': runway.number(default=0.45, min=0, max=1, step=0.05),
-    'light_intensity': runway.number(default=0.45, min=0, max=1, step=0.05),
-    'light_source_height': runway.number(default=0.45, min=0, max=1, step=0.05),
-    'gamma_correction': runway.number(default=0.45, min=0, max=1, step=0.05),
-    'stroke_density_clipping':runway.number(default=0.45, min=0, max=1, step=0.05),
-    'light_color_red': runway.number(default=0.45, min=0, max=1, step=0.05),
-    'light_color_green': runway.number(default=0.45, min=0, max=1, step=0.05),
-    'light_color_blue': runway.number(default=0.45, min=0, max=1, step=0.05)
+    'light_intensity': runway.number(default=0.85, min=0, max=1, step=0.05),
+    'light_source_height': runway.number(default=1, min=0, max=1, step=0.05),
+    'gamma_correction': runway.number(default=1, min=0, max=1, step=0.05),
+    'stroke_density_clipping':runway.number(default=1.2, min=0, max=2, step=0.05),
+    'light_color_red': runway.number(default=1, min=0, max=1, step=0.05),
+    'light_color_green': runway.number(default=1, min=0, max=1, step=0.05),
+    'light_color_blue': runway.number(default=1, min=0, max=1, step=0.05)
 }
 
 @runway.setup(options={'checkpoint': runway.file(is_directory=True)})
